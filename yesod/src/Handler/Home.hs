21a40154415d7a1c06a7ddd638c5a3f76aa26da3
otavio <otaviolimadoss@hotmail.com>
2018-12-11 00:42:17 +0000

    Tela - Criando primeira tela

--- yesod/src/Handler/Home.hs
+++ yesod/src/Handler/Home.hs
@@ -3,11 +3,25 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Home where
 
 import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+import Prelude (read)
+
+import Import
+import Text.Lucius
+import Text.Julius
+
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = defaultLayout $ do 
+        toWidgetHead [hamlet|
+                <script src=@{StaticR js_jquery_min_js}>
+                <script src=@{StaticR js_script_js}>
+        |]
+        addStylesheet $ StaticR css_bootstrap_min_css
+        addStylesheet $ StaticR css_geral_css
+        $(whamletFile "templates/home.hamlet")
